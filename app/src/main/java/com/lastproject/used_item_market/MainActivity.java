1e66f20bdfb9b3097831d61012debe6ca4e7fd67
MindException <tourer97@naver.com>
2022-03-24 00:46:34 +0900

    0324 특정 쿼리 조회 성공

--- app/src/main/java/com/lastproject/used_item_market/MainActivity.java
+++ app/src/main/java/com/lastproject/used_item_market/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lastproject.used_item_market;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
         //파이어 베이스 데이터베이스 연동
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference();
-
-        Toast.makeText(MainActivity.this, "메인화면", Toast.LENGTH_SHORT).show();
 
         //생명주기 연습
         lifebutton = (Button)findViewById(R.id.lifecycle_button);
@@ -62,9 +65,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        testQuery();
+
+    }//onCreate() 끝
+
+    void testQuery(){
+
+
+        //쿼리를 이용하여 특정 데이터 가져오기 성공
+        Button bt_query = (Button)findViewById(R.id.query_button);
+        bt_query.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Query testQuery = myRef.child("User").orderByChild("google_email").equalTo("tourer97@sunmoon.ac.kr");
+                testQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+
+                        for(DataSnapshot ds : snapshot.getChildren()){
+
+                            System.out.println(ds.getKey());
+                            User user = ds.getValue(User.class);
+                            System.out.println( "받음: " + user.google_email);
+                            if(user.google_email.equals("tourer97@sunmoon.ac.kr")){
+
+                                Toast.makeText(MainActivity.this, "쿼리 성공", Toast.LENGTH_SHORT).show();
+
+                            }else{
+                                Toast.makeText(MainActivity.this, "쿼리 실패", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+
+                    }
+
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
+            }
+        });
+
+
+    }//testQuery
 
 
 
 
-    }
+
 }
